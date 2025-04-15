1ffd1bd95edd9aae672fcb0807ae74c366613f4c
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-04-15 17:03:42 -0400

    agregados

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, abort
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin # type: ignore
 from secrets import token_urlsafe
-import psycopg2
+import psycopg2 # type: ignore
 
 conn = psycopg2.connect(
         host="localhost",
@@ -17,23 +17,31 @@ cors = CORS(app)
 app.config['DEBUG'] = True
 app.config['CORS_HEADERS'] = 'application/json'
 
-def jsonVerify(contents: list, json):
-    for cont in contents:
-        if cont not in json:
-            print(cont) 
-            print("Missing from arguments") 
-            return False
+def requestVerify(contents: list, request):
+    if not request: return False
+    for cont in contents: 
+        if cont not in request.json: return False
     return True
         
-        
+def authVerify(token):
+    if (token == None): return False
+    cur.execute('SELECT id, email, name, lname FROM users WHERE token = \'{0}\''.format(token))
+    res = cur.fetchall()
+    if (len(res) == 0): return False
+    return True
+
+def getUserId(token):
+    cur.execute('SELECT id, email, name, lname FROM users WHERE token = \'{0}\''.format(token))
+    res = cur.fetchall()
+    return res[0][0]
 
 @app.route('/auth/login/email', methods=['POST', 'OPTIONS'])
 @cross_origin(origin="*")
 def email_login():
+    if not requestVerify(['email', 'password'], request):
+        return jsonify({"detail": "Insufficient arguments"}), 400
     json = request.json
     print(json)
-    if not request or not jsonVerify(['email', 'password'], json):
-        return jsonify({"detail": "Insufficient arguments"}), 400
     cur.execute('SELECT id, token FROM users WHERE email = %s AND password = %s', (json["email"], json["password"]))
     res = cur.fetchall()
     print(res)
@@ -51,7 +59,7 @@ def register():
     token = token_urlsafe()
     json = request.json
     print(request.json)
-    if not request or not jsonVerify(['email', 'password', 'name', 'lname'], json):
+    if not requestVerify(['email', 'password', 'name', 'lname'], json):
         return jsonify({"detail": "Insufficient arguments"}), 400
     cur.execute('SELECT email FROM users WHERE email = \'{0}\''.format(json["email"]))
     res = cur.fetchall()
@@ -77,11 +85,12 @@ def register():
 @app.route('/users/self', methods=['GET', 'OPTIONS'])
 @cross_origin(origin="*")
 def self():
-    token: str = request.headers.get('Authorization').split()[1]
-    if (token == None): return jsonify({"detail": "No token"}), 400
+    try: token: str = request.headers.get('Authorization').split()[1]
+    except: return jsonify({"detail": "No token"}), 400
+    if not authVerify(token):
+        return jsonify({"detail": "Invalid token"}), 400
     cur.execute('SELECT id, email, name, lname FROM users WHERE token = \'{0}\''.format(token))
     res = cur.fetchall()
-    if (len(res) == 0): return jsonify({"detail": "Invalid token"}), 400
     return jsonify({
         "email": res[0][1],
         "id": res[0][0],
@@ -95,7 +104,7 @@ def prod():
      prods = []
      page = 0
      if ("page" in request.args): page = request.args["page"]
-     cur.execute('SELECT id, name, description, price, discount, discount_type, stock, date_added FROM products limit 5 offset 5*{0}'.format(page))
+     cur.execute('SELECT id, name, description, price, discount, discount_type, stock, date_added, brand FROM products limit 5 offset 5*{0}'.format(page))
      res = cur.fetchall()
      for prod in res:
          prods.append({
@@ -106,11 +115,88 @@ def prod():
              "discount": prod[4],
              "discount_type": prod[5],
              "stock": prod[6],
-             "date_added": prod[7]
+             "date_added": prod[7],
+             "brand": prod[8]
              })
      print(prods)    
      return jsonify(prods), 200
+ 
+@app.route('/products/get', methods=['GET'])
+@cross_origin(origin="*")
+def prod_get():
+     id = 0
+     if ("id" in request.args): id = request.args["id"]
+     else: return jsonify({"detail": "No id"}), 400
+     cur.execute('SELECT id, name, description, price, discount, discount_type, stock, date_added, brand FROM products WHERE id = {0}'.format(id))
+     res = cur.fetchall()
+     return jsonify({
+             "id": res[0][0],
+             "name": res[0][1],
+             "description": res[0][2],
+             "price": res[0][3],
+             "discount": res[0][4],
+             "discount_type": res[0][5],
+             "stock": res[0][6],
+             "date_added": res[0][7],
+             "brand": res[0][8]
+             }), 200
      
+@app.route('/users/cart/add', methods=['POST'])
+@cross_origin(origin="*")
+def cart_add():
+    try: token: str = request.headers.get('Authorization').split()[1]
+    except: return jsonify({"detail": "No token"}), 400
+    if not authVerify(token):
+        return jsonify({"detail": "Invalid token"}), 400
+    if not requestVerify(['id'], request):
+        return jsonify({"detail": "Insufficient arguments"}), 400
+    cur.execute('SELECT id FROM carts WHERE userid = {0} AND paid = \'N\''.format(getUserId(token)))
+    res = cur.fetchall()
+    currentCart = res[0][0]
+    cur.execute('SELECT id FROM cart_entries WHERE cartid = {0} AND productid = {1}'.format(currentCart, request.json["id"]))
+    res = cur.fetchall()
+    if (len(res) == 0):
+        cur.execute('INSERT INTO cart_entries (cartId, productId) VALUES ({0}, {1})'.format(currentCart, request.json["id"]))
+    else:
+        cur.execute('UPDATE cart_entries SET quantity = quantity + 1 WHERE id = {0}'.format(res[0][0]))
+    conn.commit()
+    cur.execute('SELECT id, cartid, productid, quantity FROM cart_entries WHERE cartid = {0} AND productid = {1}'.format(currentCart, request.json["id"]))
+    res = cur.fetchall()
+    return jsonify({
+        "id": res[0][0],
+        "cartid": res[0][1],
+        "productid": res[0][2],
+        "quantity": res[0][3]
+        }), 200
+
+@app.route('/users/cart', methods=['GET'])
+@cross_origin(origin="*")
+def cart():
+    try: token: str = request.headers.get('Authorization').split()[1]
+    except: return jsonify({"detail": "No token"}), 400
+    if not authVerify(token):
+        return jsonify({"detail": "Invalid token"}), 400
+    cur.execute('SELECT id FROM carts WHERE userid = {0}'.format(getUserId(token)))
+    res = cur.fetchall()
+    currentCart = res[0][0]
+    cur.execute('SELECT id, cartid, productid, quantity FROM cart_entries WHERE cartid = {0}'.format(currentCart))
+    res = cur.fetchall()
+    items = []
+    for item in res:
+        items.append({
+        "id": item[0],
+        "cartid": item[1],
+        "productid": item[2],
+        "quantity": item[3]
+        })
+    return jsonify(items), 200
+
+'''
+@app.route('/users/cart', methods=['DELETE'])
+@cross_origin(origin="*")
+def cart_delete()
+'''  
+    
 
 if __name__ == '__main__':
     app.run(host='192.168.0.18', debug=True)
--- db_def.sql
+++ db_def.sql
@@ -10,11 +10,16 @@ CREATE TABLE users (id serial PRIMARY KEY,
 					role varchar(10) NOT NULL,
 					token varchar(50) NOT NULL,
 					date_added date DEFAULT CURRENT_TIMESTAMP);
+					country varchar(50),
+					state varchar(50),
+					address string
+					);
 drop table users cascade;
 select * from users;
 
 CREATE TABLE products (id serial PRIMARY KEY,
                        name varchar(100) NOT NULL,
+                       brand varchar(100) NOT NULL,
 					   description text DEFAULT '',
 					   price decimal(12,2) NOT NULL,
 					   discount decimal(12,2) NOT NULL DEFAULT 0,
@@ -34,19 +39,26 @@ CREATE TABLE carts (id serial PRIMARY KEY,
 drop table carts cascade;
 select * from carts;
 
-CREATE TABLE cart_entry (id serial PRIMARY KEY,
+CREATE TABLE cart_entries (id serial PRIMARY KEY,
                          cartid serial REFERENCES carts(id) NOT NULL,
 						 productid serial REFERENCES products(id) NOT NULL,
-						 quantity integer DEFAULT 0
+						 quantity integer DEFAULT 1
 						 );
+						 
+drop table cart_entries cascade;
+select * from cart_entries;
+insert into cart_entries (cartid, productid) values (1, 4);
+insert into cart_entries (cartid, productid) values (1, 1);
+update cart_entries set quantity = quantity + 1 where id = 1;
+
 SELECT id, name, description, price, discount, discount_type, stock, date_added FROM products limit 5 offset 5*0;
-insert into products (name, price) VALUES ('Nintendo Switch', 300);
-insert into products (name, price) VALUES ('Nintendo Switch Lite', 250);
-insert into products (name, price) VALUES ('Nintendo Switch OLED', 350);
-insert into products (name, price) VALUES ('Nintendo Switch 2', 450);
-insert into products (name, price) VALUES ('Nintendo Switch 2 + Mario Kart World', 500);
-insert into products (name, price) VALUES ('Mario Kart World (S2)', 80);
-insert into products (name, price) VALUES ('Donkey Kong Bananza (S2)', 70);
-insert into products (name, price) VALUES ('The Legend of Zelda: Tears of the Kingdom (Switch)', 70);
-insert into products (name, price) VALUES ('The Legend of Zelda: Breath of the Wild (Switch)', 60);
-insert into products (name, price) VALUES ('Super Smash Bros. Ultimate (Switch)', 300);
+insert into products (name, brand, description, price) VALUES ('Nintendo Switch', 'Nintendo', 'Consola de videojuegos', 300);
+insert into products (name, brand, description, price) VALUES ('Nintendo Switch Lite', 'Nintendo', 'Consola de videojuegos', 250);
+insert into products (name, brand, description, price) VALUES ('Nintendo Switch OLED', 'Nintendo', 'Consola de videojuegos', 350);
+insert into products (name, brand, description, price) VALUES ('Nintendo Switch 2', 'Nintendo', 'Consola de videojuegos', 450);
+insert into products (name, brand, description, price) VALUES ('Nintendo Switch 2 + Mario Kart World', 'Nintendo', 'Consola de videojuegos', 500);
+insert into products (name, brand, description, price) VALUES ('Mario Kart World (S2)', 'Nintendo', 'Videojuegos', 80);
+insert into products (name, brand, description, price) VALUES ('Donkey Kong Bananza (S2)', 'Nintendo', 'Videojuegos', 70);
+insert into products (name, brand, description, price) VALUES ('The Legend of Zelda: Tears of the Kingdom (Switch)', 'Nintendo', 'Videojuegos', 70);
+insert into products (name, brand, description, price) VALUES ('The Legend of Zelda: Breath of the Wild (Switch)', 'Nintendo', 'Videojuegos', 60);
+insert into products (name, brand, description, price) VALUES ('Super Smash Bros. Ultimate (Switch)', 'Nintendo', 'Videojuegos', 300);
